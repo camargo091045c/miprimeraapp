945c7d3e8e7f6f6d2a5e06c94f37b4cc51559a37
edwin camargo <camargo091c@gmail.com>
2020-11-10 21:44:44 -0500

    fin de seccion 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
   constructor() {}
-
+ngOnInit()
+{
+  let numero=10;
+  numero=20;
+  
+  numero=numero+10;
+ debugger;
+  //console.log('hola edwin');
+  console.log(numero);
+}
 }
